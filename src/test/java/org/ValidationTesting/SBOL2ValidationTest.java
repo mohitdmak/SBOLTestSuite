c497e73ffdeb12e4c2ecd6066c92435d2fe92674
mehersam <mehersam251@gmail.com>
2018-09-03 18:18:18 -0700

    updated test suite

--- src/test/java/org/ValidationTesting/SBOL2ValidationTest.java
+++ src/test/java/org/ValidationTesting/SBOL2ValidationTest.java
@@ -98,14 +98,14 @@ public class SBOL2ValidationTest {
 				// File(errorFileName)));
 
 				for (String error : SBOLReader.getErrors()) {
-					System.out.println(error);
+					//System.out.println(error);
 					// bw.write(error);
 					// bw.write("\n");
 				}
 
 
 				for (String error : SBOLValidate.getErrors()) {
-					System.out.println(error);
+					//System.out.println(error);
 
 					// bw.write(error);
 					// bw.write("\n");
@@ -114,6 +114,7 @@ public class SBOL2ValidationTest {
 				// bw.close();
 				fail();
 			}
+			
 		}
 
 	}
@@ -167,7 +168,7 @@ public class SBOL2ValidationTest {
 				throws IOException, SBOLConversionException, SBOLValidationException {
 			SBOLReader.setKeepGoing(true);
 			SBOLDocument doc = SBOLReader.read(file);
-			SBOLValidate.validateSBOL(doc, true, false, false);
+			SBOLValidate.validateSBOL(doc, false, true, false);
 
 			if (SBOLReader.getNumErrors() > 0 || SBOLValidate.getNumErrors() > 0) {
 				System.out.println("Test Fail for SBOL2_ic " + file.getName());
@@ -246,7 +247,7 @@ public class SBOL2ValidationTest {
 				throws IOException, SBOLConversionException, SBOLValidationException {
 			SBOLReader.setKeepGoing(true);
 			SBOLDocument doc = SBOLReader.read(file);
-			SBOLValidate.validateSBOL(doc, false, true, false);
+			SBOLValidate.validateSBOL(doc, false, false, false);
 
 			if (SBOLReader.getNumErrors() > 0 || SBOLValidate.getNumErrors() > 0) {
 				System.out.println("Test Fail for SBOL2_nc " + file.getName());
@@ -325,89 +326,10 @@ public class SBOL2ValidationTest {
 				throws IOException, SBOLConversionException, SBOLValidationException {
 			SBOLReader.setKeepGoing(true);
 			SBOLDocument doc = SBOLReader.read(file);
-			SBOLValidate.validateSBOL(doc, false, false, true);
+			SBOLValidate.validateSBOL(doc, true, true, false);
 
 			if (SBOLReader.getNumErrors() > 0 || SBOLValidate.getNumErrors() > 0) {
 				System.out.println("Test Fail for SBOL2_bp " + file.getName());
-				// String fileName = dir + "/" + file.getName();
-				// SBOLWriter.write(doc, fileName);
-				// String errorFileName = dir + "/" + file.getName().replace(".xml", "") +
-				// "_errors.txt";
-				// BufferedWriter bw = new BufferedWriter(new FileWriter(new
-				// File(errorFileName)));
-
-				for (String error : SBOLReader.getErrors()) {
-					System.out.println(error);
-					// bw.write(error);
-					// bw.write("\n");
-				}
-
-				for (String error : SBOLValidate.getErrors()) {
-					System.out.println(error);
-
-					// bw.write(error);
-					// bw.write("\n");
-				}
-
-				// bw.close();
-				fail();
-			}
-		}
-
-	}
-
-	@RunWith(Parameterized.class)
-	public static class SBOL2False {
-		private File file;
-
-		/**
-		 * @param file
-		 *            - file to test
-		 */
-		public SBOL2False(File file) {
-			this.file = file;
-		}
-
-		/**
-		 * @return a set of files to test
-		 */
-		@Parameterized.Parameters
-		public static java.util.Collection<File> files() {
-			File file_base = null;
-			java.util.Collection<File> col = new HashSet<File>();
-
-			try {
-				file_base = new File(SBOL2ValidationTest.class.getResource("/SBOL2_false/").toURI());
-			} catch (URISyntaxException e1) {
-				e1.printStackTrace();
-			}
-			for (File f : file_base.listFiles()) {
-				col.add(f);
-			}
-
-			return col;
-		}
-
-		/**
-		 * Run each xml/XML file under the "Validation" sub-directory to test SBOL
-		 * validation exceptions.
-		 * 
-		 * @throws IOException
-		 *             see {@link IOException}
-		 * @throws SBOLConversionException
-		 *             see {@link SBOLConversionException}
-		 * @throws SBOLValidationException
-		 *             see {@link SBOLValidationException}
-		 */
-		@Test
-		public void runValidation()
-				throws IOException, SBOLConversionException, SBOLValidationException {
-			SBOLReader.setKeepGoing(true);
-			SBOLDocument doc = SBOLReader.read(file);
-			SBOLValidate.validateSBOL(doc, false, false, false);
-
-			if (SBOLReader.getNumErrors() > 0 || SBOLValidate.getNumErrors() > 0) {
-				System.out.println("Test Fail for SBOL2_false " + file.getName());
 				// String fileName = dir + "/" + file.getName();
 				// SBOLWriter.write(doc, fileName);
 				// String errorFileName = dir + "/" + file.getName().replace(".xml", "") +
